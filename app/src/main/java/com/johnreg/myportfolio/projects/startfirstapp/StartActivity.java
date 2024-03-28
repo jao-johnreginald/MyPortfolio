46b218e6627879f33e183cc9a44e78c61767ea59
John Reg <johnreginaldjao@gmail.com>
2024-03-28 23:23:32 +0800

    created StartActivity

---
+++ app/src/main/java/com/johnreg/myportfolio/projects/startfirstapp/StartActivity.java
@@ -0,0 +1,16 @@
+package com.johnreg.myportfolio.projects.startfirstapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.johnreg.myportfolio.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
