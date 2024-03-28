24feb29f612b27d212a19729598036a91efa6610
John Reg <johnreginaldjao@gmail.com>
2024-03-29 01:53:16 +0800

    created QuoteActivity

---
+++ app/src/main/java/com/johnreg/myportfolio/firstprojects/QuoteActivity.java
@@ -0,0 +1,16 @@
+package com.johnreg.myportfolio.firstprojects;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.johnreg.myportfolio.R;
+
+public class QuoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quote);
+    }
+}
