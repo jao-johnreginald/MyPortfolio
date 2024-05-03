e514bb89c2e002629e76901387f5a0c6ef4463e8
John Reg <johnreginaldjao@gmail.com>
2024-05-04 04:02:59 +0800

    added NavController

--- app/src/main/java/com/johnreg/myportfolio/MainActivity.kt
+++ app/src/main/java/com/johnreg/myportfolio/MainActivity.kt
@@ -1,69 +1,28 @@
 package com.johnreg.myportfolio
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.johnreg.myportfolio.databinding.ActivityMainBinding
-import com.johnreg.myportfolio.firstprojects.BmiActivity
-import com.johnreg.myportfolio.firstprojects.ConverterActivity
-import com.johnreg.myportfolio.firstprojects.QuoteActivity
-import com.johnreg.myportfolio.firstprojects.StartActivity
-import com.johnreg.myportfolio.models.Item
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
-    private val projectsAdapter by lazy { ProjectsAdapter(getProjects()) }
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        binding.rvProjects.adapter = projectsAdapter
+        navController = (supportFragmentManager.findFragmentById(
+            R.id.fragmentContainerView
+        ) as NavHostFragment).navController
 
-        projectsAdapter.setOnClickListener(object : ProjectsAdapter.OnClickListener {
-            override fun onClick(position: Int) {
-                val intentList = getIntents()
-                if (position < intentList.size) startActivity(intentList[position])
-            }
-        })
+        setupActionBarWithNavController(navController)
     }
 
-    private fun getProjects(): List<Item> = listOf(
-        Item(
-            "Start",
-            "Our very first project, the default 'Hello World' app!",
-            R.drawable.getting_started
-        ),
-        Item(
-            "Quote Project",
-            "Making a simple change to the layout, with my own personal motivational quote for software developers.",
-            R.drawable.quote
-        ),
-        Item(
-            "BMI Calculator",
-            "A real life working BMI calculator teaching me Variables, Methods and Conditional Logic.",
-            R.drawable.weight_scale
-        ),
-        Item(
-            "Inches to Meters Converter",
-            "My own personal calculator app I made on my own to convert inches to meters.",
-            R.drawable.ruler
-        ),
-        Item(
-            "Reginald's Restaurant",
-            "A menu app for a fictional restaurant, learning about Activities, Classes & Objects, Arrays, Intents and ListViews.",
-            R.drawable.restaurant
-        )
-    )
-
-    private fun getIntents(): List<Intent> = listOf(
-        Intent(this, StartActivity::class.java),
-        Intent(this, QuoteActivity::class.java),
-        Intent(this, BmiActivity::class.java),
-        Intent(this, ConverterActivity::class.java)
-    )
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 
 }
