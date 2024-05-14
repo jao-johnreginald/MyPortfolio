1a365532f328396270925647c77c66568180574b
John Reg <johnreginaldjao@gmail.com>
2024-05-14 09:46:59 +0800

    rearranged imports

--- app/src/main/java/com/johnreg/myportfolio/FirstFragment.kt
+++ app/src/main/java/com/johnreg/myportfolio/FirstFragment.kt
@@ -1,10 +1,10 @@
 package com.johnreg.myportfolio
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.johnreg.myportfolio.databinding.FragmentFirstBinding
 import com.johnreg.myportfolio.models.FirstProjects
