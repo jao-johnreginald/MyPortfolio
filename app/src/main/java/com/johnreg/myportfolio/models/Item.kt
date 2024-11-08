affbec745772414408a97af32ca6dafe2f34aea8
John Reg <johnreginaldjao@gmail.com>
2024-11-09 03:50:01 +0800

    refactored comment

--- app/src/main/java/com/johnreg/myportfolio/models/Item.kt
+++ app/src/main/java/com/johnreg/myportfolio/models/Item.kt
@@ -1,21 +1,21 @@
 package com.johnreg.myportfolio.models
 
+/**
+ * Drawables, like layouts files and layout IDs, are 'resources'
+
+ * Android studio takes care of creating these in the 'R' place
+
+ * They are referred in Java code as R.drawable.name_of_the_file
+
+ * When android studio takes the stuff in the 'resources' folder, it puts them into Java code
+ * and gives them a name that is converted into numbers like 6942069
+
+ * For every resource that we have, android will make a numeric ID that we can use from Java
+
+ * Long story short, for our drawable, it is a variable of type 'int'
+ */
 data class Item(
     val name: String,
     val description: String,
     val image: Int
 )
-
-/**
-
- * Drawables, like layouts files and layout IDs, are 'resources'
- * Android studio takes care of creating these in the 'R' place
- * They are referred in Java code as R.drawable.name_of_the_file
-
-When android studio takes the stuff in the 'resources' folder, it puts them into Java code
-and gives them a name that is converted into numbers like 6942069.
-
- * For every resource that we have, android will make a numeric ID that we can use from Java
- * Long story short, for our drawable, it is a variable of type 'int'
-
- */
